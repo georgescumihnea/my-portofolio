a5dcbd5f70852e0a5794411649ae1631c75784df
Mihnea Georgescu <mihtitan@gmail.com>
2024-11-19 01:36:54 +0200

    Updated projects

--- components/PortofolioProjectsCard.tsx
+++ components/PortofolioProjectsCard.tsx
@@ -6,25 +6,25 @@ import { ExternalLink } from "lucide-react";
 export function PortfolioProjectsCard() {
   const projects = [
     {
-      title: "E-commerce Platform",
+      title: "ProxyGate Landing Page",
       image: "/images/ecommerce-platform.jpg",
-      techStack: ["React", "Next.js", "Shopify API"],
+      techStack: ["Next.js", "TailwindCSS", "Framer Motion", "TypeScript"],
       description:
-        "A fully customized e-commerce solution for a retail brand, integrated with Shopify and optimized for high conversion rates.",
+        "Designed and developed a robust web platform using Next.js, enabling users to purchase and manage API access efficiently. The application features a dynamic, SEO-friendly landing page and a secure user dashboard accessible through unique tokens. Users can seamlessly view and manage their API keys, ensuring smooth integration and a user-friendly experience.",
     },
     {
-      title: "Landing Page Builder",
+      title: "Game Landing Page",
       image: "/images/landing-page-builder.jpg",
       techStack: ["Next.js", "TailwindCSS", "Framer Motion"],
       description:
-        "A drag-and-drop builder for creating optimized landing pages, complete with custom templates and payment integration.",
+        "A landing page for a indie game, featuring a dynamic and responsive design, and a seamless user experience.",
     },
     {
-      title: "Project Management Tool",
+      title: "Linktree Clone",
       image: "/images/project-management.jpg",
-      techStack: ["React", "Node.js", "MongoDB"],
+      techStack: ["Next.js", "TailwindCSS", "TypeScript", "Supabase"],
       description:
-        "A collaborative project management tool featuring real-time task updates and team communication channels.",
+        "A clone of Linktree, featuring a dynamic and responsive design, and a seamless user experience.With Supabase for authentication and database management. Users can create, edit, and delete their links, and view their linktree page. It also has web analytics to track the performance of each link.",
     },
   ];
 
